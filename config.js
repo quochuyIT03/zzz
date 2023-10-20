a84dbbf9d457edc537a7e9731961b51f2c2c7305
huy nguyen <nguyenquochuy3102@gmail.com>
2023-10-20 15:03:49 +0700

    s

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Gửi người anh yêu",
+    introTitle: 'Hoàng Mai',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương :3 `,
+    btnIntro: 'Yêu nhiều lắm <3',
+    title: 'Phải chăng anh đã yêu em từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say em từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu lắm',
+    btnNo: 'Không bao giờ :3',
+    question: 'Tại sao pé lại yêu anh nhờ :3 ',
+    btnReply: 'Nhập rùi đó :3',
+    reply: 'Yêu anh',
+    mess: 'Anh biết mà 🥰. Anh yêu em rất nhiều 😘😘',
+    messDesc: '20-10 rồi... Anh chúc người anh yêu những gì tốt đẹp nhất :3',
+    btnAccept: 'Dạ!!',
+    messLink: 'traiTym.html' 
+}
